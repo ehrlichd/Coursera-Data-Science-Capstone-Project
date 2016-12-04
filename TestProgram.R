54a170099615ce9be6981a99c2110b48e8228ea0
Pradeep K. Pant <pantpradeep@gmail.com>
2016-12-04 11:23:28 +0530

    adding projectData to gitignore

--- TestProgram.R
+++ TestProgram.R
@@ -1,6 +1,5 @@
-# R programs to practice intermedite logic which I will try to use in Capstone project 
-## Author: Pradeep K. Pant, ppant@cpan.org
-## Date: 22/08/2016
+### Download dataset from swiftkey repository
+
 
 ## Load CRAN modules 
 library(downloader)
@@ -14,7 +13,9 @@ library(tm)
 if(!file.exists("./projectData")){
   dir.create("./projectData")
 }
+
 Url <- "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip"
+
 ## Check if zip has already been downloaded in projectData directory?
 if(!file.exists("./projectData/Coursera-SwiftKey.zip")){
   download.file(Url,destfile="./projectData/Coursera-SwiftKey.zip",mode = "wb")
@@ -23,38 +24,34 @@ if(!file.exists("./projectData/Coursera-SwiftKey.zip")){
 if(!file.exists("./projectData/final")){
   unzip(zipfile="./projectData/Coursera-SwiftKey.zip",exdir="./projectData")
 }
+
 # Once the dataset is downloaded start reading it as this a huge dataset so we'll read line by line only the amount of data needed
 # before doing that lets first list all the files in the directory
+
+
 ## List all the files of /final/en_US Dataset folder
 path <- file.path("./projectData/final" , "en_US")
-files<-list.files(path, recursive=TRUE)
-# Lets make a file connection of the twitter data set
-con <- file("./projectData/final/en_US/en_US.twitter.txt", "r") 
-lineTwitter<-readLines(con) 
-# Test code
-##length(con)
-## Read the first line of text 
-#line<-readLines(con, 1) 
-## Read the next line of text 
-#line<-readLines(con, 1) 
-## Read in the next 5 lines of text 
-#line<-readLines(con, 5) 
-#length(readLines(con))
+files <- list.files(path, recursive=TRUE)
+
 
 # Code for capastone quiz 1
 # 1. The en_US.blogs.txt  file is how many megabytes?
-file.size("en_US.blogs.txt")
+file.size(paste(path,"en_US.blogs.txt",sep="/"))
 # [1] 210160014 ~ 200 MB
 # 2. The en_US.twitter.txt has how many lines of text?
 # Step:
-length(lineTwitter)
-## [1] 2360148
+length(paste(path,lineTwitter,sep="/"))
+## [1] 2,360,148
 
 # 3. What is the length of the longest line seen in any of the three en_US data sets?
 
-# Step: Read in the lines to arrays:
+# FIRST READ IN THE LINES THEMSELVES
+
 # Twitter data
+con <- file("./projectData/final/en_US/en_US.twitter.txt", "r")
+lineTwitter <- readLines(con)
 longTwitter<-length(lineTwitter)
+close(con)
 
 # Blog data
 con <- file("./projectData/final/en_US/en_US.blogs.txt", "r")
@@ -65,23 +62,20 @@ longBlogs<-length(lineBlogs)
 con <- file("./projectData/final/en_US/en_US.news.txt", "r")
 lineNews<-readLines(con) 
 longNews<-length(lineNews)
+close(con)
 
-# Need the longest line in each array.
-longBlogs = nchar(longBlogs)
-max(nchar(longBlogs))
-# [1] 1
-#Apparently below is max of lineBlogs
+# max of lineBlogs
 require(stringi)
 longBlogs<-stri_length(lineBlogs)
 max(longBlogs)
 # [1] 40835
 
-#Apparently below is max of lineNews
+# max of lineNews
 longNews<-stri_length(lineNews)
 max(longNews)
 # [1] 5760
 
-# Apparently below is max of lineTwitter
+# max of lineTwitter
 longTwitter<-stri_length(lineTwitter)
 max(longTwitter)
 # [1] 213
@@ -89,10 +83,10 @@ max(longTwitter)
 # 4. In the en_US twitter data set, if you divide the number of lines where the word "love" (all lowercase) occurs 
 # by the number of lines the word "hate" (all lowercase) occurs, about what do you get?
 
-loveTwitter<-grep("love",lineTwitter)
-length(loveTwitter)
+loveTwitter<-grep("love",lineTwitter) ## Find indeces wher the word occurs
+
 hateTwitter<-grep("hate",lineTwitter)
-length(hateTwitter)
+length(loveTwitter)/length(hateTwitter) ##dive the lengths of indeces
 
 # 5. The one tweet in the en_US twitter data set that matches the word "biostats" says what?
 biostatsTwitter<-grep("biostats",lineTwitter)
